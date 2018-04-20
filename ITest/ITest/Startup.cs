7cc2374bad72ca4b4071fde3f6afaef4b4132f7b
Unknown <marti_stamatov@abv.bg>
2018-04-20 11:23:05 +0300

    Fixed startup

--- ITest/ITest/Startup.cs
+++ ITest/ITest/Startup.cs
@@ -25,45 +25,72 @@ namespace ITest
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
+            Environment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ITestDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            this.RegisterData(services);
+            this.RegisterAuthentication(services);
+            this.RegisterServices(services);
+            this.RegisterInfrastructure(services);
 
-            services.AddIdentity<User, IdentityRole>()
-                .AddEntityFrameworkStores<ITestDbContext>()
-                .AddDefaultTokenProviders();
-
-
-        
-
+        }
+        private void RegisterServices(IServiceCollection services)
+        {
             services.AddTransient<ICategoriesService, CategoriesService>();
-            services.AddTransient<IEmailSender, EmailSender>();
-            
-
-            // Add application services.
-
-            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
-            services.AddScoped<ISaver, EFSaver>();
-
-            // services.AddMvc();
-         
             services.AddTransient<IQuestionService, QuestionService>();
+            services.AddTransient<IEmailSender, EmailSender>();
+        }
 
+        private void RegisterInfrastructure(IServiceCollection services)
+        {
+            services.AddMvc();
             services.AddAutoMapper();
 
             services.AddScoped<IMappingProvider, MappingProvider>();
-            services.AddMvc();
+        }
+        private void RegisterAuthentication(IServiceCollection services)
+        {
+
+            services.AddIdentity<User, IdentityRole>()
+               .AddEntityFrameworkStores<ITestDbContext>()
+               .AddDefaultTokenProviders();
+
+            if (this.Environment.IsDevelopment())
+            {
+                services.Configure<IdentityOptions>(options =>
+                {
+                    // Password settings
+                    options.Password.RequireDigit = false;
+                    options.Password.RequiredLength = 3;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequiredUniqueChars = 0;
+
+                    // Lockout settings
+                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(1);
+                    options.Lockout.MaxFailedAccessAttempts = 999;
+                });
+            }
         }
 
+        private void RegisterData(IServiceCollection services)
+        {
+            services.AddDbContext<ITestDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+            services.AddScoped<ISaver, EFSaver>();
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, RoleManager<IdentityRole> roleManager)
         {
@@ -79,6 +106,7 @@ namespace ITest
             }
 
             app.UseAuthentication();
+            //The seeding of roles
             UserRoleInitializer.SeedRoles(roleManager);
             app.UseStaticFiles();
 
