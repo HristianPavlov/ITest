0a12791618b8c40669d3c12cfa12c4733f4360a5
Martin Stamatov <marti_stamatov@abv.bg>
2018-04-22 21:42:21 +0300

    Clicked answers are now active class

---
+++ ITest/ITest/wwwroot/js/clickableAnswers.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    const activeClass = 'active';
+    //const firstItem = document.getElementById('categoryButton-0');
+    //firstItem.classList.add(activeClass);
+    //$('#startButton').text(firstItem.textContent);
+    //let currentQuestion;
+    let currentAnswer;
+    let currentClass;
+
+    let testId = 11;
+    $('.label-text').on('click', function () {
+
+        currentAnswer = document.getElementById(this.id);
+        currentClass = currentAnswer.className.split(' ')[1];
+         $(`.${currentClass}`).removeClass(activeClass);
+        currentAnswer.classList.add(activeClass);
+    });
+});
