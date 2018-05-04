41effe45311231a9249aa51121b934b43a466ac1
Unknown <hristian.p.pavlov@abv.bg>
2018-05-04 12:10:47 +0300

    Editing tests
    S

--- ITest/ITest.Services.Data/CreateTestService.cs
+++ ITest/ITest.Services.Data/CreateTestService.cs
@@ -16,8 +16,8 @@ namespace ITest.Services.Data
         private readonly IRepository<Question> questions;
         private readonly IRepository<Test> tests;
         private readonly IRepository<Answer> answers;
-        
-     
+
+
 
 
         public CreateTestService(ISaver saver, IMappingProvider mapper, IRepository<Answer> answers, IRepository<Question> questions, IRepository<Test> tests)
@@ -36,40 +36,15 @@ namespace ITest.Services.Data
             this.saver.SaveChanges();
         }
 
-        public void Update(TestEditDTO dto) 
-        {
-
-
-            var TestUpdate = this.tests.All.Include(x=> x.Questions)
-                .ThenInclude(q=>q.Answers)
+        public void Update(TestEditDTO dto)
+        {            
+            var TestUpdate = this.tests.All.Include(x => x.Questions)
+                .ThenInclude(q => q.Answers)
                 .FirstOrDefault(x => x.Name == dto.Name);
-                //.Include(t=>t.Category)
-                //.Include(t => t.Questions)
-                //    .ThenInclude(q=>q.Answers)
-              //  ;/*.AsNoTracking()*/
-
-           var id = TestUpdate.Id;
-            //var cartegoryID = TestUpdate.CategoryId;
-            //var cat= TestUpdate.Category;
-
-            TestUpdate=    this.mapper.MapTo(dto,TestUpdate);
+          
+            var id = TestUpdate.Id;
+            TestUpdate = this.mapper.MapTo(dto, TestUpdate);
             TestUpdate.Id = id;
-            //TestUpdate.Id = id;
-            //TestUpdate.CategoryId = cartegoryID;
-            //  TestUpdate.Category = cat;
-            //TestUpdate.Category.Id = cat.Id;
-            //TestUpdate.Category.Name = cat.Name;
-            //TestUpdate.Category.Tests = cat.Tests;
-
-
-
-
-            // var qqq = this.mapper.MapTo<Test>(dto);
-            //TestUpdate.TimeInMinutes = dto.TimeInMinutes;
-            //TestUpdate.Status = dto.Status;
-
-            //TestUpdate.Questions = dto.Questions;
-
 
             this.tests.Update(TestUpdate);
             this.saver.SaveChanges();
